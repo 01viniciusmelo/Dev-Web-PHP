d77ca6514678c1d8991a1a649bdcf017e93f13b9
Rafael Carvalho <rapha.pse@outlook.com>
2016-11-06 22:42:34 -0200

    Meu Web-sistema

---
+++ Sistemas/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>SiS - Rafael Carvalho</title>
+    <link rel="stylesheet" type="text/css" href="style/stylo.css">
+</head>
+<body>
+
+<nav id="menu">
+    <ul>
+        <li>a</li>
+        <li>b</li>
+        <li>c</li>
+    </ul>
+</nav>
+
+<div id="tema">
+<img src="img/logo.png">
+</div>
+
+</body>
+</html>
