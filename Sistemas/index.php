8ddc8dc5d1e312392fb5ae9ac462e890d4b90a14
Rafael Carvalho <rapha.pse@outlook.com>
2016-11-07 14:49:13 -0200

    alterações no projeto

--- Sistemas/index.php
+++ Sistemas/index.php
@@ -3,21 +3,63 @@
 <head>
     <meta charset="UTF-8">
     <title>SiS - Rafael Carvalho</title>
-    <link rel="stylesheet" type="text/css" href="style/stylo.css">
+    <link rel="stylesheet" type="text/css" href="estilo/estilo.css">
 </head>
 <body>
 
-<nav id="menu">
+<nav class="menu">
     <ul>
-        <li>a</li>
-        <li>b</li>
-        <li>c</li>
+        <li><a href="index.php">HOME</a></li>
+        <li><a href="projeto.php" >PROJETOS</a></li>
+        <li><a href="sobre.php">SOBRE</a></li>
+        <li><a href="contato.php">CONTATOS</a></li>
     </ul>
+    <div id="tema">
+        <a href="index.php"><img src="img/fire.png"></a>
+    </div>
 </nav>
 
-<div id="tema">
-<img src="img/logo.png">
-</div>
+<article id="art">
+    <div id="pcp">
+        <img src="img/frame.png">
+
+        <h1>Desenvolvimento de WEB-SITE</h1>
+
+        <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos,
+        e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos
+        e os embaralhou para fazer um livro de modelos de tipos.</p>
+        <p>Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica,
+            permanecendo essencialmente inalterado.</p>
+        <p>Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum,
+        e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+
+    </div>
+
+    <div id="sub">
+        <h1>Linguagens de programação</h1>
+
+        <p>
+            É um fato conhecido de todos que um leitor se distrairá com o conteúdo de texto legível de uma página
+            quando estiver examinando sua diagramação.
+        </p>
+        <p>
+            A vantagem de usar Lorem Ipsum é que ele tem uma distribuição normal de letras, ao contrário de
+            "Conteúdo aqui, conteúdo aqui", fazendo com que ele tenha uma aparência
+            similar a de um texto legível.
+        </p>
+        <p>
+            Muitos softwares de publicação e editores de páginas na internet agora usam
+            Lorem Ipsum como texto-modelo padrão, e uma rápida busca por 'lorem ipsum' mostra vários websites ainda em
+            sua fase de construção.
+        </p>
+        <p>
+            Várias versões novas surgiram ao longo dos anos,eventualmente por acidente, e às vezes de propósito
+            (injetando humor, e coisas do gênero).
+        </p>
+    </div>
+
+</article>
+
 
 </body>
 </html>
